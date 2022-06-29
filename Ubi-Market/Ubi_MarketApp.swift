af01d27feeb3937be36f15e7ca6905844bafc414
Ye Hangyu <32982019+HangyuYe@users.noreply.github.com>
2022-06-29 21:24:56 +0800

    Initial commit

---
+++ Ubi-Market/Ubi_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ubi_MarketApp.swift
+//  Ubi-Market
+//
+//  Created by Jerry on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Ubi_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
